e9f21480d0c4273acce7c9182aa33cc9558487f5
kemalyayil <kemalyayil@gmail.com>
2021-07-22 21:00:32 -0300

    Explicit Wait-Visibility of Element

---
+++ src/Day10/_01_VisibilityOfElement.java
@@ -0,0 +1,28 @@
+package Day10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class _01_VisibilityOfElement {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\zeynep\\Google Drive\\Selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://the-internet.herokuapp.com/dynamic_loading/1");
+
+        WebDriverWait wait= new WebDriverWait(driver,10);       // sadece saniye ile calisir
+
+        WebElement startButton = driver.findElement(By.cssSelector("#start>button"));
+        startButton.click();
+
+        WebElement finishText = driver.findElement(By.id("finish"));
+        wait.until(ExpectedConditions.visibilityOf(finishText));
+        String result = finishText.getText();
+
+        System.out.println(result);
+    }
+}
