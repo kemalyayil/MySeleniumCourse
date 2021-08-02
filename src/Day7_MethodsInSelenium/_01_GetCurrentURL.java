c69b8533df1b16519268188cd44ea9b3a811470b
kemalyayil <kemalyayil@gmail.com>
2021-08-02 18:41:49 -0300

    GetURL

--- src/Day7_MethodsInSelenium/_01_GetCurrentURL.java
+++ src/Day7_MethodsInSelenium/_01_GetCurrentURL.java
@@ -1,8 +1,6 @@
-package Day7;
+package Day7_MethodsInSelenium;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class _01_GetCurrentURL {
